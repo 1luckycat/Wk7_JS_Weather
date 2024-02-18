6468d402219c878a939ffae5a0cf1960a79e7238
1luckycat <027.mott@gmail.com>
2024-02-18 10:42:16 -0600

    trying to add code for unsplash image

--- DOM/js/main.js
+++ DOM/js/main.js
@@ -4,6 +4,8 @@ let form = document.querySelector('#weather-form');
 let getCity = "";
 const API_KEY = 'e6df94833f7b3cebec300444948cd589'
 
+
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     getCity = document.querySelector('#city').value;
@@ -25,7 +27,7 @@ const DOMElements = {
     "weather" : ".weather-info"
 }
 
-const createList = (city, temp, high, low, forecast, humidity, sunrise) => {
+const createList = (city, temp, high, low, forecast, humidity) => {
     const html = `<div id=${city} class='table d-flex justify-content-center' style="width: 18rem;">
     <table class="table">
         <thead>
@@ -53,9 +55,35 @@ const createList = (city, temp, high, low, forecast, humidity, sunrise) => {
     document.querySelector(DOMElements['weather']).insertAdjacentHTML('beforeend', html);
 }
 
+
 const loadData = async () => {
     const info = await getData();
         createList(info.name, info.main.temp, info.main.temp_max, info.main.temp_min, info.weather[0].main, info.main.humidity);
 
 };
 
+function SearchPhotos() {
+    let clientId = "diuBWEGUJSqaoD6BxQCDJn_0_Q472hVdG5a3571fIw4"
+    let query = document.getElementById("city").value
+    let url = "https://api.unsplash.com/search/photos?client_id="+clientId+"&query="+ query;
+
+    fetch(url)
+        .then(function (data) {
+            return data.json();
+
+        })
+        .then(function(data) {
+            console.log(data);
+            let result = data.results[0].urls.small
+
+            
+            // data.results.forEach(photo => {
+            //     let result = `
+            //     <img src="${photo.urls.small}">
+            //     <a href="${photo.links.download}">
+            // `;
+            $('#result').append(result);
+        })
+};
+
+// })};
